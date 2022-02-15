e4bf51737149498addcf934b5854210bc52ed11e
k.kolesnikov <k.kolesnikov@profsoft.pro>
2022-02-15 13:54:40 +0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Кирилл Колесников on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
